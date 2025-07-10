a7f2a4867d07c509ed0a5dabe7133f88c80551ab
Gokul Kartha <kartha.gokul@gmail.com>
2025-07-10 18:44:09 +0200

    Fixing lint

--- generate_proto.py
+++ generate_proto.py
@@ -12,16 +12,15 @@ terms of the Apache License Version 2.0 which is available at
 SPDX-License-Identifier: Apache-2.0
 """
 
-from grpc_tools import protoc
 import os
-import google.protobuf
+from grpc_tools import protoc
+import pkg_resources
 
 PROTO_DIR = "up-spec/up-core-api"
 OUTPUT_DIR = "."
 TRACK_FILE = "generated_proto_files.txt"
 
 # Add google protobuf include path
-import pkg_resources
 PROTOBUF_INCLUDE = pkg_resources.resource_filename('grpc_tools', '_proto')
 
 
@@ -36,19 +35,20 @@ def generate_all_protos():
             if file.endswith(".proto"):
                 proto_file = os.path.join(root, file)
                 print(f"Compiling {proto_file}...")
+
                 result = protoc.main([
                     '',
                     f'-I{PROTO_DIR}',
-                    f'-I{PROTOBUF_INCLUDE}',   # <--- Add this
+                    f'-I{PROTOBUF_INCLUDE}',
                     f'--python_out={OUTPUT_DIR}',
                     f'--grpc_python_out={OUTPUT_DIR}',
                     proto_file
                 ])
+
                 if result != 0:
                     print(f"Failed to compile {proto_file}")
                 else:
                     print(f"Compiled {proto_file} successfully.")
-
                     base_name = os.path.splitext(file)[0]
                     rel_dir = os.path.relpath(root, PROTO_DIR)
                     output_dir = os.path.join(OUTPUT_DIR, rel_dir)
@@ -57,7 +57,7 @@ def generate_all_protos():
                     generated_files.append(os.path.join(output_dir, f"{base_name}_pb2_grpc.py"))
 
     # Ensure __init__.py in all generated proto folders
-    for root, dirs, files in os.walk(OUTPUT_DIR):
+    for root, dirs, _ in os.walk(OUTPUT_DIR):
         init_file = os.path.join(root, '__init__.py')
         if not os.path.exists(init_file):
             open(init_file, 'a').close()
@@ -69,6 +69,7 @@ def generate_all_protos():
         for path in generated_files:
             f.write(f"{path}\n")
     print(f"Tracked {len(generated_files)} generated files in {TRACK_FILE}")
+
 
 if __name__ == "__main__":
     generate_all_protos()
